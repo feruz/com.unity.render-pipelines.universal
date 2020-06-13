0d0862aa01d6aa469062711666e605262292c8a7
oltim1 <too.burn@gmail.com>
2020-06-14 00:18:09 +0300

    Added tilemap Shadow support

--- Runtime/2D/ShadowCaster2D.cs
+++ Runtime/2D/ShadowCaster2D.cs
@@ -82,35 +82,38 @@ namespace UnityEngine.Experimental.Rendering.Universal
             return m_ApplyToSortingLayers != null ? Array.IndexOf(m_ApplyToSortingLayers, layer) >= 0 : false;
         }
 
-        private void Awake()
-        {
-            if(m_ApplyToSortingLayers == null)
+        private Vector3 vec2To3(Vector2 inputVector) {
+            return new Vector3(inputVector.x, inputVector.y, 0);
+        }
+        private void Awake() {
+            if (m_ApplyToSortingLayers == null)
                 m_ApplyToSortingLayers = SetDefaultSortingLayers();
 
             Bounds bounds = new Bounds(transform.position, Vector3.one);
-            
+
             Renderer renderer = GetComponent<Renderer>();
-            if (renderer != null)
-            {
+            if (renderer != null) {
                 bounds = renderer.bounds;
-            }
-            else
-            {
+            } else {
                 Collider2D collider = GetComponent<Collider2D>();
                 if (collider != null)
-                    bounds = collider.bounds;
+                    if (collider.GetType() == typeof(PolygonCollider2D)) {
+                        m_ShapePath = Array.ConvertAll<Vector2, Vector3>(((PolygonCollider2D)collider).GetPath(0), vec2To3);
+                        m_UseRendererSilhouette = false;
+                    } else {
+                        bounds = collider.bounds;
+                    }
             }
 
             Vector3 relOffset = bounds.center - transform.position;
 
-            if (m_ShapePath == null || m_ShapePath.Length == 0)
-            {
+            if (m_ShapePath == null || m_ShapePath.Length == 0) {
                 m_ShapePath = new Vector3[]
                 {
-                    relOffset + new Vector3(-bounds.extents.x, -bounds.extents.y),
-                    relOffset + new Vector3(bounds.extents.x, -bounds.extents.y),
-                    relOffset + new Vector3(bounds.extents.x, bounds.extents.y),
-                    relOffset + new Vector3(-bounds.extents.x, bounds.extents.y)
+                        relOffset + new Vector3(-bounds.extents.x, -bounds.extents.y),
+                        relOffset + new Vector3(bounds.extents.x, -bounds.extents.y),
+                        relOffset + new Vector3(bounds.extents.x, bounds.extents.y),
+                        relOffset + new Vector3(-bounds.extents.x, bounds.extents.y)
                 };
             }
         }
